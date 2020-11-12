81a6caa2b24c25b45c1b1b03b6b99f501ae56726
Adam Pan <adampan94@gmail.com>
2020-11-11 17:21:53 -0800

    Added back client folder with vue3

---
+++ client/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <test-request/>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import TestRequest from "./components/TestRequest.vue";
+
+export default defineComponent({
+  name: "App",
+  components: {
+    TestRequest
+  }
+});
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+</style>
---
+++ client/src/components/TestRequest.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="container">
+    Hi, this is a boilerplate
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "TestRequest",
+});
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
